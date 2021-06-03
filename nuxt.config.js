dfd345a7e1d576ae4eec574159fdae1559b9473e
mizphses <fuminori@mizphses.com>
2021-06-03 21:00:46 +0900

    Init

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,15 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '行動する知性。' },
+      { hid: 'og:site_name', property: 'og:site_name', content: '行動する知性。' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://mizphses.github.io/chisei' },
+      { hid: 'og:title', property: 'og:title', content: '行動する知性。' },
+      { hid: 'og:description', property: 'og:description', content: '行動する知性。' },
+      { hid: 'og:image', property: 'og:image', content: 'https://mizphses.github.io/chisei/ogp.png' },
+      { name: 'twitter:card', content: 'summary_large_image' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
