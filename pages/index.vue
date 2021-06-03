35aa1c3fd7ccf9362386fb2ba1bc4ad83529f8c6
mizphses <fuminori@mizphses.com>
2021-06-03 23:35:16 +0900

    追加

--- pages/index.vue
+++ pages/index.vue
@@ -9,17 +9,17 @@ export default {
   data() {
     return{
       nounlist: ["知性", "未来", "過去", "想像", "多摩キャン", "夢", "ツイッター", "教授", "電車", "バス", "CPlus", "Manaba", "ジンギスカン", "モノレール", "Temico", "多摩", "多摩動物公園", "学費", "基礎マクロ経済学", "テミス", "信用", "知性", "知性", "中央大学", "フォレゲ", "iTL", "Cスクエア", "学生", "インスタ", "Webex", "インターン", "労働", "鉛筆", "バイト", "クラウドキャンパス", "Word", "Excel", "Powerpoint", "性別"],
-      verblist: ["死亡", "行動", "絶起", "過労", "忘却", "落下", "喪失", "獲得", "登録", "落単", "会話", "増加", "遊戯", "停止", "変化", "編集", "殺害", "惹起", "克服", "過誤", "錯誤", "盗聴", "見落し", "引受", "掃出", "放送", "祈願", "脱出", "追越", "エンカ", "要求", "咀嚼", "束縛", "鳴動", "夢想", "含有", "所持", "据付", "入手", "性交"],
+      verblist: ["死亡", "行動", "絶起", "過労", "忘却", "落下", "喪失", "獲得", "登録", "落単", "会話", "増加", "遊戯", "停止", "変化", "編集", "殺害", "惹起", "克服", "過誤", "錯誤", "盗聴", "見落し", "引受", "掃出", "放送", "祈願", "脱出", "追越", "エンカ", "要求", "咀嚼", "束縛", "鳴動", "夢想", "含有", "所持", "据付", "散財", "課金", "入手", "獣医学部", "性交"],
     }
   },
   computed: {
     randomize1() {
-      const random = Math.round(Math.random() * 39)
-      const verblist = ["Die", "Action", "Oversleep", "Overwork", "Forget", "Drop", "Miss", "Obtain", "Subscribe", "Fail", "Talk", "Increase", "Play", "Stop", "Change", "Edit", "Kill", "Arise", "Overcome", "Mistake", "Misunderstand", "Overhear", "Overlook", "Undergo", "Swipe", "Broadcast", "Pray", "Quit", "Overtake", "Encounter", "Seek", "Bite", "Bind", "Ring", "Dream", "Contain", "Belong", "Put", "Gain", "Sex"];
+      const random = Math.round(Math.random() * 40)
+      const verblist = ["Die", "Action", "Oversleep", "Overwork", "Forget", "Drop", "Miss", "Obtain", "Subscribe", "Fail", "Talk", "Increase", "Play", "Stop", "Change", "Edit", "Kill", "Arise", "Overcome", "Mistake", "Misunderstand", "Overhear", "Overlook", "Undergo", "Swipe", "Broadcast", "Pray", "Quit", "Overtake", "Encounter", "Seek", "Bite", "Bind", "Ring", "Dream", "Contain", "Belong", "Put", "Spending", "Billing", "Gain", "Faculty of Veterinary Medicine", "Sex"];
       return [random, verblist[random]]
     },
     randomize2() {
-      const random2 = Math.round(Math.random() * 38)
+      const random2 = Math.round(Math.random() * 40)
       const nounlist = ["Knowledge", "Future", "Past", "Imagine", "Tama", "Dream", "Twitter", "Professor", "Train", "Bus", "CPlus", "Manaba", "Genghis Khan", "Monorail", "Temico", "Tama", "Zoo", "Tuition", "Kiso Macro", "Themis", "Credit", "Knowledge", "Knowledge", "Chuo", "FGW", "iTL", "C Square", "Students", "Instagram", "Webex", "Intern", "Working", "Pencil", "Part Time", "Cloud Campus", "Word", "Excel", "Powerpoint", "SEX"];
       return [random2, nounlist[random2]]
     },
