4f2c8dad447a0c05e235218c6b736b4a8d71f4de
なと <info@gentoo.app>
2021-06-04 16:59:41 +0000

    Yarn

--- components/koudousuru.vue
+++ components/koudousuru.vue
@@ -91,7 +91,7 @@
     <div v-else-if="randomize2[1] == 'Webex'">
       <div>
         <a href="http://www.zoom.us/">
-          <img src="/zoom.png" />
+          <img src="/assets/zoom.png" />
         </a>
         <h1 class="title">
           {{ verblist[randomize1[0]] }}する{{ nounlist[randomize2[0]] }}。
